6562a5728ff0fa4cfda684a22d2edce7dc07bf1e
Victor Bittencourt <victor.bittencourt@waycarbon.com>
2017-11-24 19:51:13 -0200

    Things setup for Heroku

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 4000;
+
 var app = express();
 var maintenance = false;
 
@@ -53,6 +55,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(4000, () => {
-  console.log('Server is up on port 4000');
+app.listen(port, () => {
+console.log(`Server is up on port ${port}`);
 });
