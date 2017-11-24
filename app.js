13fcc5f012cf248abb11a84a7289a16cf55a0451
Victor Bittencourt <victor.bittencourt@waycarbon.com>
2017-11-24 19:10:24 -0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+var maintenance = false;
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + "\n");
+  next();
+});
+
+app.use((req, res, next) => {
+  if (maintenance) {
+    res.render('maintenance.hbs');
+  }
+  next();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Welcome Page',
+    welcomeMsg: 'Welcome to my test website!'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    code: '333',
+    errMsg: 'Something went wrong'
+  });
+});
+
+app.listen(4000, () => {
+  console.log('Server is up on port 4000');
+});
